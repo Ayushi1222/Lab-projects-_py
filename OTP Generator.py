6652533d9a3e0a9fc8575b5defa4b6d179d64a02
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2023-05-02 19:45:15 +0530

    Update OTP Generator.py

--- OTP Generator.py
+++ OTP Generator.py
@@ -3,7 +3,7 @@ import random
 num=string.digits
 alpha_num=string.digits+string.ascii_letters
 print("OTP GENERATOR!")
-c=input("Enter 'an' for alphanumeric OTP or 'n' for OTP od digits only: ")
+c=input("Enter 'an' for alphanumeric OTP or 'n' for OTP of digits only: ")
 if c=='an':
     otp_ls=random.choices(alpha_num,k=6)
     otp=''.join(otp_ls)
